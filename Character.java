b5617407864f828eee91217d96945faac76352e0
joliejyss <101139208+joliejyss@users.noreply.github.com>
2022-06-02 14:00:12 -0400

    Animated character and gummy. Score and game over label

---
+++ Character.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Character here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Character extends Actor
+{
+    /**
+     * Act - do whatever the Character wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if (Greenfoot.isKeyDown("a"))
+        {
+            setLocation (getX()-2, getY());
+        }
+        if (Greenfoot.isKeyDown("d"))
+        {
+            setLocation (getX()+2, getY());
+        }
+        if (Greenfoot.isKeyDown("w"))
+        {
+            setLocation (getX(), getY()-2);
+        }
+        if (Greenfoot.isKeyDown("s"))
+        {
+            setLocation (getX(), getY()+2);
+        }
+        
+        eat();
+    }
+    
+    public void eat()
+    {
+        if (isTouching(Gummy.class))
+        {
+            removeTouching(Gummy.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.increaseScore();
+            world.spawnGummy();
+        }
+    }
+}
+
+
--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    public int score = 0;
+    public Label scoreLabel = new Label(score, 100);
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,6 +17,35 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false);
+        
+        Character hi = new Character();
+        addObject (hi, 300, 350);
+        
+        spawnGummy();
+        
+        addObject (scoreLabel, 50, 50);
+        
+    }
+    
+    public void spawnGummy()
+    {
+        int x = Greenfoot.getRandomNumber(600);
+        int y = Greenfoot.getRandomNumber(400);
+        
+        Gummy qq = new Gummy();
+        addObject(qq, x, y);
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label ("Game Over!" , 100);
+        addObject (gameOverLabel, 300, 200);
     }
 }
+
